958b917faae80d12f1de7145b316b9b235ff5f7c
Zig Hooker <zighooker@gmail.com>
2023-08-23 14:47:33 -0700

    Clean up sketchpad build function & add custom sizing

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const sketchContainer = document.querySelector('.sketchContainer');
 
 function buildSketchpad(size) {
-    let divSize = String(Math.floor(500 / Math.sqrt(size))) + "px";
-
-    for (let i = 0; i < size; i++) {
+    // clears out previous sketchpad
+    if (sketchContainer.hasChildNodes()) {
+        const previousSketch = Array.from(sketchContainer.childNodes);
+        previousSketch.forEach((child) => {
+            sketchContainer.removeChild(child);
+        });
+    }
+    
+    // creates new sketchpad
+    let divSize = String((512 / size) + "px");
+    for (let i = 0; i < Math.pow(size, 2); i++) {
         const sketchDiv = document.createElement('div');
         sketchDiv.style.width = divSize;
         sketchDiv.style.height = divSize;
@@ -14,7 +22,13 @@ function buildSketchpad(size) {
 }
 
 function colorIn () {
-    
+
 }
+
+// buttons to select sketchpad size
+const selectButtons = Array.from(document.getElementsByClassName('sizeSelectButton'));
+selectButtons.forEach((button) => {
+    button.addEventListener('click', () => buildSketchpad(button.id));
+});
 
 buildSketchpad(16);
